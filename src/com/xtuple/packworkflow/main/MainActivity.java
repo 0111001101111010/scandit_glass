cda91bf7f40e1581a4a79281ca5df77e634f0e5d
stanzheng <dev.stanzheng@gmail.com>
2014-05-20 23:21:02 -0400

    adding in onActivity Result for Barcodes

--- src/com/xtuple/packworkflow/main/MainActivity.java
+++ src/com/xtuple/packworkflow/main/MainActivity.java
@@ -1,5 +1,9 @@
 package com.xtuple.packworkflow.main;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.android.glass.app.Card;
 
 import android.app.Activity;
@@ -15,11 +19,17 @@ import android.view.WindowManager;
 import com.google.android.glass.touchpad.GestureDetector;
 import com.google.android.glass.touchpad.Gesture;
 import com.loopj.android.http.*;
+import com.xtuple.XtupleRestClient;
+
 import org.json.*;
 public class MainActivity extends Activity {
 
 // create variable instances
-
+	public List<String> barcodes  = new ArrayList<String>();
+	public List<String> descriptions  = new ArrayList<String>();
+	public List<String> uuids  = new ArrayList<String>();
+	public String obj;
+	
     private GestureDetector mGestureDetector;
 	private static final int SCANDIT_CODE_REQUEST =2;
 	protected void onCreate(Bundle savedInstanceState){
@@ -28,13 +38,9 @@ public class MainActivity extends Activity {
 	    getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
 
-		Card card1 = new Card(this);
-		card1.setText("Welcome to PackWorkflow!!");
-		card1.setFootnote("xTuple");
-		View card1View = card1.getView();
-		setContentView(card1View);
 		//javascript that
 		final Activity that = this;
+	    makeCard(this,"Hello","World");
 		makeRequest(that);
 		
 		mGestureDetector = createGestureDetector(this);
@@ -64,6 +70,14 @@ public class MainActivity extends Activity {
             }
         });
 
+	};
+	
+	void makeCard(Activity that, String body, String footer){
+		Card card1 = new Card(that);
+		card1.setText(body);
+		card1.setFootnote(footer);
+		View card1View = card1.getView();
+		setContentView(card1View);
 	};
 /**
  * Boiler plate google code
@@ -99,15 +113,146 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (requestCode == SCANDIT_CODE_REQUEST && resultCode == RESULT_OK){
-       		Log.d("before","SCANDIT_CODE_REQUEST");
+      		Log.d("before","SCANDIT_CODE_REQUEST");
     		String contents = data.getStringExtra("SCAN_RESULT");
            	Log.d("after","SCANDIT_CODE_REQUEST");
+		try {
+			String check = XtupleRestClient.onPacklist(obj,contents);
+			//its on the list
+			if(!check.equals("NOPE")){
+                //startActivity(new Intent(this, StopStopWatchActivity.class));
+    				//selected
+            RequestParams params = new RequestParams();
+            params.put("uuid", check);
+            params.put("quantity", "1");
+            
             Card newCard = new Card(this);
             newCard.setImageLayout(Card.ImageLayout.FULL);
-    		newCard.setText(contents);
+    		newCard.setText("Issued");
     		newCard.setFootnote("xTuple");
     		View card1View1 = newCard.getView();
-    		setContentView(card1View1);
+    		setContentView(card1View1);	
+    		
+               	Log.d("start","");
+           //WebRequest.issueOrder(new AsyncHttpResponseHandler(),params);
+            final Activity foo = this;
+          WebRequest.issueOrder(new AsyncHttpResponseHandler() {
+        	            @Override
+        	            public void onStart() {
+        	                // Initiated the request
+        	                Card newCard = new Card(foo);
+        	                newCard.setImageLayout(Card.ImageLayout.FULL);
+        	        		newCard.setText("Issuing....");
+        	        		newCard.setFootnote("xTuple");
+        	        		View card1View1 = newCard.getView();
+        	        		setContentView(card1View1);
+        		               	Log.d("start","");
+        	            }
+
+        	            @Override
+        	            public void onSuccess(String response) {
+        	                // Successfully got a response
+        	            }
+
+        	            @Override
+        	            public void onFailure(Throwable e, String response) {
+        	                // Response failed :(
+        	            }
+
+        	            @Override
+        	            public void onFinish() {
+        	                // Completed the request (either success or failure)
+        		    	        WebRequest.getOrders(new AsyncHttpResponseHandler() {
+        		    	            @Override
+        		    	            public void onStart() {
+        		    	                // Initiated the request
+
+        		    	                Card newCard = new Card(foo);
+        		    	                newCard.setImageLayout(Card.ImageLayout.FULL);
+        		    	        		newCard.setText("Loading Next Line Item....");
+        		    	        		newCard.setFootnote("xTuple");
+        		    	        		View card1View1 = newCard.getView();
+        		    	        		setContentView(card1View1);
+        		    		               	Log.d("start","");
+        		    		                mSpeech.speak("Loading Next Line Item....", TextToSpeech.QUEUE_FLUSH, null);
+        		    	            }
+
+        		    	            @Override
+        		    	            public void onSuccess(String response) {
+        		    	                // Successfully got a response
+        		    	            	obj = response;
+        		    	            	if (!"No Issuable Items!".equals(response)){
+        		    	                    try{
+        		    	                        barcodes  = XtupleRestClient.getIssueToShippingAtShipping(response);
+        		    	                        descriptions = XtupleRestClient.getIssueToShippingDescriptions(response);       
+        		    	                      // uuids = XtupleRestClient.getOrderUUIDs();
+        		    	                        uuids = XtupleRestClient.getOrderUUIDs(response);
+        		    	                                                    
+        		    	                        //add them
+        		    			                Card newCard = new Card(foo);
+        		    			                newCard.setImageLayout(Card.ImageLayout.FULL);
+        		    		                    newCard.setText(descriptions.get(0));
+        		    			        		newCard.setFootnote("xTuple");
+        		    			        		View card1View1 = newCard.getView();
+        		    			        		setContentView(card1View1);
+        		    			        		
+        		    			               mSpeech.speak("Next Line Item Ready", TextToSpeech.QUEUE_FLUSH, null);
+        		    	                    }
+        		    	                    catch (Exception e) {
+        		    	         				// TODO Auto-generated catch block
+        		    	            			throw new RuntimeException(e);
+        		    	         				
+        		    	         			}
+        		    	                 }
+        		    	                 else{
+        		    	         		    //mlsText.add("No Issuable Items!");
+        		    		                Card newCard = new Card(foo);
+        		    		                newCard.setImageLayout(Card.ImageLayout.FULL);
+        		    	                     newCard.setText("Order Complete, No more Issuable Items!");
+        		    		        		newCard.setFootnote("xTuple");
+        		    		        		View card1View1 = newCard.getView();
+        		    		        		setContentView(card1View1);
+        		    		                mSpeech.speak("Order Complete, No more Issuable Items!", TextToSpeech.QUEUE_FLUSH, null);
+        		    	                 }		
+        		    	            	
+        		    	            		
+        		    	            }
+
+        		    	            @Override
+        		    	            public void onFailure(Throwable e, String response) {
+        		    	                // Response failed :(
+        		    	            	try {
+        		    						throw e;
+        		    					} catch (Throwable e1) {
+        		    						// TODO Auto-generated catch block
+        		    						e1.printStackTrace();
+        		    					}
+        		    	               //	Log.d("fail",response);
+        		    	            }
+
+        		    	            @Override
+        		    	            public void onFinish() {
+        		    	               	Log.d("finish","");
+        		    	                // Completed the request (either success or failure)
+        		    	            }
+        		    	        });
+        	            }
+        	        }, params);
+                Log.v("@@@@","IssueToShipping");				
+			}
+			else{
+			//it isn't
+    	        Card newCard = new Card(this);
+    	        newCard.setImageLayout(Card.ImageLayout.FULL);
+    			newCard.setText("That line item is not on this order, please check again");
+				newCard.setFootnote("xTuple");
+				View card1View1 = newCard.getView();
+				setContentView(card1View1);	
+			}
+			} catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e);	
+		}
 	    }
 
 	    super.onActivityResult(requestCode, resultCode, data);
@@ -170,4 +315,16 @@ public class MainActivity extends Activity {
         }
         return false;
     }
+    
+
+//	@Override
+//	protected void onActivityResult(int requestCode, int resultCode,
+//	        Intent data) {
+//		if (requestCode == SCANDIT_CODE_REQUEST && resultCode == RESULT_OK){
+//           //scandit activity
+// 
+//	    } 
+//	    super.onActivityResult(requestCode, resultCode, data);
+//	}
+
 }
